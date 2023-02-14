b0549e530353459eb30ffbccfd8a14295af0914c
draganbuhin <35523573+draganbuhin@users.noreply.github.com>
2023-02-14 12:06:14 +0100

    Create prvi.php

---
+++ prvi.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Pozdrav <b>Antune</b>, prijatelj!";
+
+echo date("D M j G:i:s T Y"); 
+$var="";
+
+for ($i = 1; $i <= 100; $i++) {
+	
+	$var.="*";
+    echo $var."<br>";
+}
+
+echo "kraj"
+
+?>
